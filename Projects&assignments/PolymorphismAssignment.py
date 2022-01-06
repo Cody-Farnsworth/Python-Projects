41de3be9b2ee6517fbc37ce9030a5d08bd7ed36a
Cody-Farnsworth <86331200+Cody-Farnsworth@users.noreply.github.com>
2022-01-05 18:04:29 -0800

    assignment and file managment

---
+++ Projects&assignments/PolymorphismAssignment.py
@@ -0,0 +1,72 @@
+
+
+#parent class
+class User:
+    name= "Cody"
+    email= "cody@gmail.com"
+    password= "123456"
+
+    def getLoginImfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_password = input("Enter your password: ")
+        if (entry_email == self.email and entry_password == self.password):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The password or email is incorrect.")
+
+#Child Class Empoyee
+class Employee(User):
+    base_pay = 11.00
+    department = "General"
+    pin_number = "3980"
+
+    #This is the same method in the parent class
+    #The diffrence is that, instead of using password , we're using the pin
+
+    def getLoginImfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_pin = input("Enter your pin: ")
+        if (entry_email == self.email and entry_password == self.password):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The password or email is incorrect."
+
+class vendor(User):
+    vendor_type = "Food"
+    vendor_company = "Pepsi"
+    vendor_id = "1456_PI"
+
+    #This is the same method in the parent class
+    #The diffrence is that, instead of using password , we're using the id.
+
+    def getLoginImfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_pin = input("Enter your pin: ")
+        if (entry_email == self.email and entry_password == self.password):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The password or email is incorrect."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
